b241cab5e6525bc87fb7369739017279a95b8872
Asha <asha@MacBook-Air.fritz.box>
2023-11-03 12:23:17 +0100

    adding -redirects for Netlify

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './contexts/user.context';
-import { ProductsProvider } from './contexts/products.context';
+import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
 
@@ -18,11 +18,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>        
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>      
     </BrowserRouter>
   </React.StrictMode> 
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -9,7 +9,7 @@ import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
 
 import { signOutUser } from  '../../utils/firebase/firebase.utils';
-import './navigation.styles.scss';
+import {NavigationContainer, NavLinks, NavLink, LogoContainer} from './navigation.styles';
 const Navigation = () => {
   const { currentUser } = useContext(UserContext); 
   const { isCartOpen } = useContext(CartContext);
@@ -17,27 +17,27 @@ const Navigation = () => {
   // console.log(currentUser);
     return (
       <Fragment>
-        <div className='navigation'>
-          <Link className='logo-container' to= '/' >
+        <NavigationContainer>
+          <LogoContainer to= '/' >
             <div><CrwnLogo className='logo'/></div>
-          </Link> 
-          <div className='nav-links-container'>
-            <Link className='nav-link' to='/shop' >
+          </LogoContainer> 
+          <NavLinks>
+            <NavLink to='/shop' >
                 SHOP
-            </Link>
+            </NavLink>
             {currentUser ? (
-                <span className='nav-link' onClick={signOutUser}>
+                <NavLink as = 'span' onClick={signOutUser}>
                   SIGN OUT
-                </span>
+                </NavLink>
                 ) : (
-                  <Link className='nav-link' to='/auth' >
+                  <NavLink to='/auth' >
                     SIGN IN
-                  </Link>
+                  </NavLink>
               )}
               <CardIcon />
-          </div>
+          </NavLinks>
          { isCartOpen && <CartDropdown />}
-        </div>
+        </NavigationContainer>
         <Outlet />
       </Fragment>
     )
